0071c3ab946bcfd1c0310bd1a3ff64edd62342f0
nacho1960 <ignacio1riveros@gmail.com>
2024-02-28 22:53:47 -0300

    pusheada para el profe

--- entregable/src/App.jsx
+++ entregable/src/App.jsx
@@ -1,17 +1,50 @@
 
 import './App.css'
 import Card from './components/card/Card'
+import { useState } from 'react';
 
 function App() {
+  const [nombre, setNombre] = useState('');
+  const [apellido, setApellido] = useState('');
+  const [show, setShow] = useState(false)
+  const [err, setErr] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if(nombre.length >= 2 && nombre.length <= 50 &&nombre.length >= 2 && apellido.length){
+        setShow(true)
+        setErr(false)
+    } else {
+        setErr(true)
+    }
+
+   
+  };
 
 
   return (
     <>
-    <div className="App">
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
-    </div>
+    <div style={{border: "3px solid black"}}>
+      <h1>formulario de inscripcion: </h1>
+      <form onSubmit={handleSubmit}>
+        <label nombre="nombre">Nombre:</label>
+        <input type="text"  value={nombre} onChange={(e) => setNombre(e.target.value)} />
+        <label apellido="apellido">Apellido:</label>
+        <input type="text"  value={apellido} onChange={(e) => setApellido(e.target.value)} />
+        <button type="submit">Enviar</button>
+      </form>
+      <Card nombre={nombre} apellido={apellido}/>
+      {show ? 
+            <>
+                <h4>Gracias, {nombre}, tus datos ingresaron correctacmente</h4>
+                
+            </>
+        : <p>ingrese sus datos.</p>
+        }
+        {err && <h4 style={{color: 'red'}}>Debe colocar la información correctamente</h4>}
+    </div >
+    
 
   
     </>
--- entregable/src/components/card/Card.jsx
+++ entregable/src/components/card/Card.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function Card() {
+function Card({nombre,apellido}) {
+ 
   return (
-    <div>SOY CARD</div>
+    <div>
+       <h2>Datos del formulario:</h2>
+       <p>Nombre: {nombre}</p>
+       <p>Apellido: {apellido}</p>
+    </div>
+    
   )
 }
 
